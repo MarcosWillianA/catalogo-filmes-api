23cb71ed7b3c37b43985b872d139ba36a361bcf5
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-21 13:56:12 -0300

    Busca
    Consegui buscar os filmes corretamente, agora é uma questão de inserir os dados nos elementos DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const textoBusca = document.querySelector('#texto-busca');
 const botaoBusca = document.querySelector('#botao-busca');
+const listaFilmesEncontrados = document.querySelector('#lista-filmes-encontrados');
 const poster = document.querySelector('#poster');
 const detalhesFilme = document.querySelector('#detalhes-filme');
 const descricaoFilme = document.querySelector('#descricao-filme');
@@ -7,27 +8,45 @@ const url = `http://www.omdbapi.com/?apikey=${chave}&`;
 const posterAPI = `http://img.omdbapi.com/?apikey=${chave}&`;
 
 async function buscarDados() {
-    if (textoBusca.value === '') {
-        alert('Por favor, digite um nome válido')
-        return;
+    if (!textoBusca.value.trim()) {
+        return null;
     } else {
         try {
             const resposta = await fetch(`${url}s=${encodeURIComponent(textoBusca.value)}`);
             const dados = await resposta.json();
-            console.log(dados.Search[0]);
-            if (!resposta.ok) {
-                throw new Error('Não achou');
+            if (!resposta.ok || !dados.Search) {
+                throw new Error('Nenhum filme encontrado');
             }
+            return dados.Search;
         }
         catch (erro) {
             console.error(erro);
+            return null;
         }
     }
 }
 
-botaoBusca.addEventListener('click', buscarDados);
+async function exibirResultadosBusca() {
+    const filmesEncontrados = await buscarDados();
+    listaFilmesEncontrados.innerHTML = '';
+    if (!filmesEncontrados || filmesEncontrados.length === 0) {
+        listaFilmesEncontrados.innerHTML = 'Nenhum filme encontrado';
+        return;
+    }
+        console.log(filmesEncontrados);
+        filmesEncontrados.forEach(filme => {
+        const itemLista = document.createElement('li');
+        const linkFilme = document.createElement('a');
+        linkFilme.innerHTML = `${filme.Title} (${filme.Year})`;
+        listaFilmesEncontrados.appendChild(itemLista);
+        itemLista.appendChild(linkFilme);
+        });
+}
+
+botaoBusca.addEventListener('click', exibirResultadosBusca);
 textoBusca.addEventListener('keydown', evento => {
     if (evento.key === 'Enter') {
-        buscarDados();
+        exibirResultadosBusca();
     }
 })
+
