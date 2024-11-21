d7141c6487785a2e5d8d012ef89a7a2eec1b47b1
Marcos Willian <marcoswillianl1987@gmail.com>
2024-11-21 12:07:19 -0300

    Js primeiros passos
    elementos dom e variaveis no js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const textoBusca = document.querySelector('#texto-busca');
+const botaoBusca = document.querySelector('#botao-busca');
+const poster = document.querySelector('#poster');
+const detalhesFilme = document.querySelector('#detalhes-filme');
+const descricaoFilme = document.querySelector('#descricao-filme');
+const url = `http://www.omdbapi.com/?apikey=[${chave}]&`;
+const posterAPI = `http://img.omdbapi.com/?apikey=[${chave}]&`;
+
